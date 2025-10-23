333a9414ad4fefac8e93e70fb944e33a0133d743
Maikellatt <maikel.latt@gmail.com>
2025-10-23 12:19:58 +0300

    first commit

--- resources/js/pages/posts/Index.vue
+++ resources/js/pages/posts/Index.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { index } from '@/routes/posts';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import { MoreVertical } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -43,11 +38,6 @@ defineProps<{
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-col gap-4 overflow-x-auto rounded-xl p-4">
             <!-- <pre>{{ posts }}</pre> -->
-            <ul>
-                <li v-for="(post, index) in posts" :key="index">
-                    {{ post.title }}
-                </li>
-            </ul>
             <Table>
                 <TableCaption>A list of your recent blog posts.</TableCaption>
                 <TableHeader>
@@ -61,31 +51,33 @@ defineProps<{
                         <TableHead>
                             <span class="sr-only">Actions</span>
                         </TableHead>
-
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     <TableRow v-for="post in posts" :key="post.id">
-                        <TableCell class="font-medium"> {{post.id}} </TableCell>
-                        <TableCell>{{post.title}}</TableCell>
-                        <TableCell>{{post.author}}</TableCell>
-                        <TableCell>{{post.created_at_formatted}}</TableCell>
-                        <TableCell>{{post.updated_at_formatted}}</TableCell>
-                        <TableCell>{{post.published}}</TableCell>
+                        <TableCell class="font-medium"> {{ post.id }} </TableCell>
+                        <TableCell>{{ post.title }}</TableCell>
+                        <TableCell>{{ post.author }}</TableCell>
+                        <TableCell>{{ post.created_at_formatted }}</TableCell>
+                        <TableCell>{{ post.updated_at_formatted }}</TableCell>
+                        <TableCell>{{ post.published }}</TableCell>
                         <TableCell class="text-right"> .00 </TableCell>
                         <TableCell>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                              <DropdownMenuSeparator />
-                              <DropdownMenuItem>Profile</DropdownMenuItem>
-                              <DropdownMenuItem>Billing</DropdownMenuItem>
-                              <DropdownMenuItem>Team</DropdownMenuItem>
-                              <DropdownMenuItem>Subscription</DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                          
+                            <div class="flex justify-end">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger as-child>
+                                        <Button size="icon" variant="ghost">
+                                            <MoreVertical />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuItem>View</DropdownMenuItem>
+                                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem class="text-destructive">Delete</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
                         </TableCell>
                     </TableRow>
                 </TableBody>
